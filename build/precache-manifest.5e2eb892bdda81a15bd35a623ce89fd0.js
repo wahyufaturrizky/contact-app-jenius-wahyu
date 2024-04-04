ac6f84f45dac908fe4c67e01d98a085e0e092e59
wahyufaturrizki <wahyufaturrizkyy@gmail.com>
2024-04-04 14:56:09 +0700

    FIX: build

---
+++ build/precache-manifest.5e2eb892bdda81a15bd35a623ce89fd0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf96a077120802309b6b09ba6c53ca40",
+    "url": "/wahyufaturrizky/contact-app-jenius-wahyuindex.html"
+  },
+  {
+    "revision": "0b17927eac5319ea27e8",
+    "url": "/wahyufaturrizky/contact-app-jenius-wahyustatic/css/2.e5b99fdb.chunk.css"
+  },
+  {
+    "revision": "dc9e2f71f35b910f41f3",
+    "url": "/wahyufaturrizky/contact-app-jenius-wahyustatic/css/main.b254e9ac.chunk.css"
+  },
+  {
+    "revision": "0b17927eac5319ea27e8",
+    "url": "/wahyufaturrizky/contact-app-jenius-wahyustatic/js/2.16056e9d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/wahyufaturrizky/contact-app-jenius-wahyustatic/js/2.16056e9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc9e2f71f35b910f41f3",
+    "url": "/wahyufaturrizky/contact-app-jenius-wahyustatic/js/main.34e630cd.chunk.js"
+  },
+  {
+    "revision": "b2673486fdd0f4d99149",
+    "url": "/wahyufaturrizky/contact-app-jenius-wahyustatic/js/runtime-main.038df2a1.js"
+  },
+  {
+    "revision": "80674f0b40500baee1891e207f8d32e4",
+    "url": "/wahyufaturrizky/contact-app-jenius-wahyustatic/media/blob1.80674f0b.svg"
+  },
+  {
+    "revision": "a53502b567c49e266fe932cca2dec4a6",
+    "url": "/wahyufaturrizky/contact-app-jenius-wahyustatic/media/blob2.a53502b5.svg"
+  },
+  {
+    "revision": "3544bae93cb2e98837a1c3bc452b57d5",
+    "url": "/wahyufaturrizky/contact-app-jenius-wahyustatic/media/usericon.3544bae9.svg"
+  }
+]);
